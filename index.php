d8af176600ffcce50f503c946ae6a39d63ff0d9c
Zach Queal <zach.queal@gmail.com>
2017-06-09 20:19:57 -0400

    Create index.php

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 <?php
 
-# Required Documents
-require 'vendor/autoload.php'); # for use with composer
+require 'vendor/autoload.php';
 
-# Configuration
 Flight::register('db', 'PDO', array('mysql:host=localhost;port=3306;dbname=database', 'username', 'password'), function($db) {
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 });
 // $db = Flight::db();
 
-# Functions
 function help(){
   echo 'This is your help documentation!';
 }
 
-# Routes
+/* routes */
 Flight::route('GET /', function(){
   echo 'Hello World!';
 });
@@ -25,14 +22,11 @@ Flight::route('POST /@name', function($name){
 
 Flight::route('/help', 'help');
 
-# Error Documents
 function notFound(){
   echo 'The resource you are trying to view is not here.';
 }
 
 Flight::map('notFound', 'notFound');
 
-# Execute
+/* execute */
 Flight::start();
-
-?>
